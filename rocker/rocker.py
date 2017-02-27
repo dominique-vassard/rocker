cf66a5423eac8c989a6abcefcfbc895a98b001e3
Dominique VASSARD <dominique.vassard@kwanko.com>
2017-02-27 19:07:29 +0100

    Add quick login

--- rocker/rocker.py
+++ rocker/rocker.py
@@ -1,26 +1,48 @@
 import click
+import subprocess
 
 
 @click.group()
-@click.option('--userpass', '-u', type=str)
 @click.pass_context
-def rocker(ctx, userpass):
+def rocker(ctx):
+    check_docker()
     ctx.obj = {}
-    ctx.obj['login'] = userpass.split(':')[0]
-    ctx.obj['pass'] = userpass.split(':')[1]
 
 
 @click.command()
+@click.option('--username', '-u', prompt=True, type=str)
+@click.option('--password', '-p', prompt=True, hide_input=True, type=str)
+@click.argument('url')
 @click.pass_context
-def login(ctx):
-    click.echo(ctx.obj)
-    click.echo('Login')
+def login(ctx, username, password, url):
+    try:
+        res = subprocess.check_output(["docker", "login",
+                                       "-u", username,
+                                       "-p", password,
+                                       url])
+        click.echo(res)
+    except subprocess.CalledProcessError:
+        raise click.Abort
 
 
 @click.command()
 @click.pass_context
 def ping(ctx):
     click.echo(ctx.obj)
+
+
+def check_docker():
+    """Check if docker is installed
+
+    Raises:
+        (click.ClickException): If docker is not installed
+    """
+    try:
+        subprocess.check_output(["which", "dockr"])
+    except subprocess.CalledProcessError:
+        msg = click.style("rocker requires docker to be installed.",
+                          fg="red", bold=True)
+        raise click.ClickException(msg)
 
 
 rocker.add_command(login)
